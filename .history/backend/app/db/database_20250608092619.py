2cc075e260a93d4d76147e2808ee03553bc50e63
root <root@DESKTOP-FROB2QC>
2025-06-09 21:52:14 +0800

    up

--- .history/backend/app/db/database_20250608092619.py
+++ .history/backend/app/db/database_20250608092619.py
@@ -19,7 +19,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # 依赖函数，用于获取数据库会话
-def get_db():
+def get_db() -> Session:
     """
     获取数据库会话，在FastAPI依赖注入中使用
     
